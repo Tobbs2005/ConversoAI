32d5f567fc5536b2199a22a033d9e9093ff236fa
Toby Fang <tobyfang05@gmail.com>
2025-05-26 01:15:24 +0800

    navbar complete

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,7 +1,7 @@
 const NewCompanion = () => {
   return (
     <div>
-      
+      stuff
     </div>
   )
 }
---
+++ app/my-journey/page.tsx
@@ -0,0 +1,9 @@
+const Profile = () => {
+  return (
+    <div>
+      jouney
+    </div>
+  )
+}
+
+export default Profile
