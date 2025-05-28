d7a6274ee260b4ff6aaa7abeb39a9314cad7b8c8
Toby Fang <tobyfang05@gmail.com>
2025-05-29 01:04:27 +0800

    implement search companions functionality

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,8 +1,29 @@
-const CompanionsLibrary = () => {
+import { getAllCompanions } from "@/lib/actions/companions.actions";
+import CompanionCard from "@/components/ui/CompanionCard";
+import SearchInput from "@/components/ui/SearchInput";
+import SubjectFilter from "@/components/ui/SubjectFilter";
+
+const CompanionsLibrary = async ({ searchParams }: SearchParams) => {
+  const filters = await searchParams;
+  const subject = filters.subject ? filters.subject : '';
+  const topic = filters.topic ? filters.topic: '';
+
+  const companions = await getAllCompanions({subject, topic});
   return (
-    <div>
-      companions
-    </div>
+    <main>
+      <section className="flex justify-between gap-4 max-sm:flex-col">
+        <h1>Companion Library</h1>
+        <div className="gap-4 flex">
+          <SearchInput/ >
+          <SubjectFilter/ >
+        </div>
+      </section>
+      <section className="companions-grid">
+        { companions.map((companion) => (
+          <CompanionCard key={companion.id} {...companion} />
+        )) }
+      </section>
+    </main>
   )
 }
 
